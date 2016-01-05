fc35214d50b1af3308ec3f08768e2e950f120118
Wane Wang <jhihguan@gmail.com>
2016-01-05 20:19:51 +0800

    Initial Commit

---
+++ Shrang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shrang
+//
+//  Created by Wane Wang on 2016/1/5.
+//  Copyright © 2016年 Wane Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
